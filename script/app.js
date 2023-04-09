8f92a5d88cc90372995de845a93fba8aeb12e7a7
Muhammad Shaheryar <125795625+muhammadshaheryar-dev@users.noreply.github.com>
2023-04-09 11:05:03 -0700

    JavaScript-Class

--- script/app.js
+++ script/app.js
@@ -1,4 +1,4 @@
 // Day 9 APR 2023 Javascript Class 1
-var zakat_amount = +prompt("Enter Your Zakat Amount")
+var zakat_amount = +prompt("Enter Your Amount")
 var percent  = 0.025*zakat_amount;
-alert(percent)
+alert("Your Total Zakat Deduction Amount"+percent)
